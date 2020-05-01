54966dd066702bc84b643d6e22f287e2d3c0d5f7
Renato <renatomarquesteles@gmail.com>
2020-05-01 14:01:58 -0300

    rotas autenticadas

---
+++ server/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '6528d7f642f41e382b64124264445199',
+    expiresIn: '1d',
+  },
+};
--- server/src/services/AuthenticateUserService.ts
+++ server/src/services/AuthenticateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
 import User from '../models/User';
 
 interface Request {
@@ -30,9 +31,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, '6528d7f642f41e382b64124264445199', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return { user, token };
